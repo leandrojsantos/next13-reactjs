68acf3f53c55cb3eeb58f8bb7fca9be3d76afb6c
leandrojsantos <ljsrsantos@gmail.com>
2023-05-07 19:03:51 -0300

    feature: test com api from github

--- test/src/app/page.tsx
+++ test/src/app/page.tsx
@@ -1,9 +1,13 @@
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const response = await fetch('http://api.github.com/users/leandrojsantos')
+  const user = await response.json();
+
   return (
     <div>
       <h1>Home</h1>
+      <pre>{JSON.stringify(user, null, 3)}</pre>
       <Link href="/dashboard">Dashboard</Link>
     </div>
   )
