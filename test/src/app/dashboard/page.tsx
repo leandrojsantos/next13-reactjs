3cce85b76785562ec463ac148a87d895418877f7
leandrojsantos <ljsrsantos@gmail.com>
2023-05-17 17:10:20 -0300

    refactor: data dashboard

--- test/src/app/dashboard/page.tsx
+++ test/src/app/dashboard/page.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link"
 
+// https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m
+
 export default async function Dashboard () {
-  const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=-23.68&longitude=-46.45&hourly=temperature_2m')
+  const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m')
   const data = await response.json();
 
   return (
     <div>
       <h1>Dashboard</h1>
-      <pre>{JSON.stringify(data, data.lon, data.lat)}</pre>
       <pre>{JSON.stringify(data, null, 3)}</pre>
       <Link href="/">Home</Link>
     </div>
